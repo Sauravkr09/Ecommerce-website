cf904bf666d11f006524641393feeadd31d4edcc
saurav kumar <105961100+Sauravkr09@users.noreply.github.com>
2025-02-19 01:20:45 +0530

    first commit

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 try {
-  mongoose.connect("mongodb+srv://souravkr9012:748688311a8@ecommerce.txy3g.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
+  mongoose.connect(
+    "mongodb+srv://souravkr9012:7488688311a@ecommerce.txy3g.mongodb.net/?retryWrites=true&w=majority&appName=ecommerce",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
+  );
   console.log("Database Connected Successfully");
 } catch (err) {
   console.log("Database Not Connected");
